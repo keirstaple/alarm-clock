cc1b2d08e3e1be4476b249ca03bd6ce553e4f1af
keir staple and timothea mcguire <student@epicodus.com>
2016-03-15 11:57:09 -0700

    add api

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -23,3 +23,17 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#time').text(moment());
 });
+
+var apiKey = "da921fa6f90ad30a2be0cfbf0e081c5d";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+      console.log(JSON.stringify(response));
+    });
+  });
+});
