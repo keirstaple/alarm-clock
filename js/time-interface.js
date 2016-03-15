8fc6df2837c728525bd047b5312d895d6502dad8
keir staple and timothea mcguire <student@epicodus.com>
2016-03-15 16:42:51 -0700

    add jquery for alarm clock

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,3 +1,1 @@
-$(document).ready(function(){
-  $('#time').text(moment());
-});
+
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,40 +1,31 @@
-var pingPong = require('./../js/ping-pong.js').pingPong;
-
 $(document).ready(function(){
-  $('#ping-pong').submit(function(event){
+  setInterval(updateTime, 1000);
+  function updateTime() {
+    $('#time').text(moment().format("h:mm:ss a"));
+  };
+
+  $('#alarmForm').submit(function(event){
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element){
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    var hour = (moment().format('hh'));
+    var minute = (moment().format('mm'));
+    var meridian = (moment().format('a'));
+    var currentTime = hour + ":" + minute + " " + meridian;
+
+    var alarmHour = $('select#hour').val();
+    var alarmMinute = $('select#minute').val();
+    var alarmMeridian = $('select#meridian').val();
+    var alarm = alarmHour + ":" + alarmMinute + " " + alarmMeridian;
+
+    setInterval(wakeUp, 1000);
+    function wakeUp() {
+      if (currentTime === alarm) {
+        alert("time to wake up!");
+      }
+      else {
+        console.log("keep sleeping zzz");
+      }
+    };
   });
 });
 
-$(document).ready(function(){
-  $('#signup').submit(function(event){
-    event.preventDefault();
-    var email = $('#email').val();
-    $('#signup').hide();
-    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
-  });
-});
 
-$(document).ready(function(){
-  $('#time').text(moment());
-});
-
-var apiKey = require('./../.env').apiKey;
-
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-       }).fail(function(error) {
-         $('.showWeather').text(error.message);
-    });
-  });
-});
