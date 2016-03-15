00b626e8c84ff16643c2307c4e0f25e6e0c2cc8c
keir staple and timothea mcguire <keirstaple@comcast.net>
2016-03-15 11:00:54 -0700

    add bower to gulpfile

--- build/js/app.js
+++ build/js/app.js
@@ -1,8 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-exports.pingPong= function(goal) {
+exports.pingPong = function(goal) {
   var output = [];
   for (var i = 1; i <= goal; i++) {
-    if (i % 15 == 0) {
+    if (i % 15 === 0) {
       output.push("ping-pong");
     } else if (i % 3 === 0) {
       output.push("ping");
@@ -13,7 +13,7 @@ exports.pingPong= function(goal) {
     }
   }
   return output;
-}
+};
 
 },{}],2:[function(require,module,exports){
 var pingPong = require('./../js/ping-pong.js').pingPong;
@@ -36,6 +36,10 @@ $(document).ready(function(){
     $('#signup').hide();
     $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
   });
+});
+
+$(document).ready(function(){
+  $('#time').text(moment());
 });
 
 },{"./../js/ping-pong.js":1}]},{},[2]);
